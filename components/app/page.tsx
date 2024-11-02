ec76c6363dfc6a260bc972bfae37073dbaf7c3b6
ismael <ismael@cordobadigitalmedia.com>
2024-11-02 10:26:48 -0600

    Update page.tsx

--- components/app/page.tsx
+++ components/app/page.tsx
@@ -10,7 +10,6 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 import { FeaturedReading } from "@/components/blog-list"
-import { FeatureList } from "@/components/features"
 import { Footer } from "@/components/footer"
 import { SiteHeader } from "@/components/site-header"
 import { WelcomeHero } from "@/components/welcome-hero"
